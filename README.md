1406ada38967659dc718a2ceee9a67170884936d
Inshiraah <syedainshiraah84@gmail.com>
2026-02-13 15:02:31 +0530

    xsab cj

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # amazon_backend
+#pip install alembic
--- models.py
+++ models.py
@@ -1,2 +1,11 @@
 from db import Base 
 #user model
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
+    address = Column(String)
+    phone = Column(String)
+    isprime = Column(Boolean)
